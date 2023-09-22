23f52930b90e034fc38d8dd25e1d9f12956c3a60
davinmetoti <110443558+DavinMetoti@users.noreply.github.com>
2023-09-22 10:27:21 +0700

    new

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './assets/style/style.css'
 import ReactDOM from 'react-dom/client';
 import NotesApps from './components/NotesApp';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,4 +10,9 @@ root.render(
         <NotesApps />
     </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example, reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
 
